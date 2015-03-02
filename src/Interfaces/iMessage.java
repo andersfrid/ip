cd4d4c128c438c727842f7d462e966fb88fc52cf
Anders Frid <anders.frid85@gmail.com>
2015-03-02 13:42:11 +0100

    Fixat interface iMessage

---
+++ src/Interfaces/iMessage.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+import javax.swing.Icon;
+
+public interface iMessage {
+	public String getMessage();
+	public void setMessage();
+	public Icon getImage();
+	public void setImage();
+
+}
