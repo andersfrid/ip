2907cd1a83fa18964370227131946a4f537367b8
Hiplobbe <hiplobbe2@hotmail.com>
2015-03-02 17:07:27 +0100

    Commented logger code.

--- src/Server/Logger.java
+++ src/Server/Logger.java
@@ -4,10 +4,20 @@ import java.io.*;
 import java.util.*;
 
 public class Logger{
-	
-	private void log(String logMessage) throws FileNotFoundException
+	/**
+	 * Saves the information to the log file.
+	 * 
+	 * @param logMessage The message to put in the file.
+	 * @throws IOException 
+	 */
+	private void log(String logMessage) throws IOException
 	{
-		PrintWriter out = new PrintWriter("log.txt");
+		File file = new File("log.txt");
+		if(!file.exists()) {
+			file.createNewFile();
+		}
+		
+		PrintWriter out = new PrintWriter(file);
 		
 		synchronized (out) {
 			out.println(logMessage);
@@ -15,13 +25,25 @@ public class Logger{
 			out.close();
 		}		
 	}
-	
-	public void logUser(String username) throws FileNotFoundException
+	/**
+	 * Logs a users login.
+	 * 
+	 * @param username
+	 * @throws IOException 
+	 */
+	public void logUser(String username) throws IOException
 	{
 		log(new Date()+username+" logged in");
 	}
-	
-	public void logMessage(String username, String toUser, String message) throws FileNotFoundException
+	/**
+	 * Logs sent information.
+	 * 
+	 * @param username The username of the user that sent the message.
+	 * @param toUser The username of the user to recieve the message.
+	 * @param message The message sent.
+	 * @throws IOException 
+	 */
+	public void logMessage(String username, String toUser, String message) throws IOException
 	{
 		if(toUser.isEmpty())
 		{
@@ -32,7 +54,12 @@ public class Logger{
 			log(new Date()+":"+username+" wrote to "+toUser+":"+message);
 		}
 	}
-
+	/**
+	 * Saves a message sent to all users.
+	 * 
+	 * @param userMessage The message to be saved.
+	 * @throws IOException
+	 */
 	public void saveAllMessage(UserMessage userMessage) throws IOException {
 	 File dir = new File("Messages");
 	 File[] directoryListing = dir.listFiles();
@@ -40,14 +67,20 @@ public class Logger{
 	 if (directoryListing != null) {
 	    for (File child : directoryListing) 
 	    {
-	    	saveToUser(userMessage, child);
+	    	saveToUserMessage(userMessage, child);
 	    }
 	  }  
 	}
-
-	private void saveToUser(UserMessage userMessage, File child)
+	/**
+	 * Saves a message sent to a user, in the designated file.
+	 * 
+	 * @param userMessage The message to be saved
+	 * @param file The file to save to.
+	 * @throws FileNotFoundException
+	 */
+	private void saveToUserMessage(UserMessage userMessage, File file)
 			throws FileNotFoundException {
-		PrintWriter out = new PrintWriter(child);
+		PrintWriter out = new PrintWriter(file);
 		
 		synchronized (out) {
 			out.println(new Date()+":"+userMessage.getUsername()+" wrote "+userMessage.getMessage());
@@ -55,16 +88,28 @@ public class Logger{
 			out.close();
 		}
 	}
-
+	/**
+	 * Saves a message that was sent to a specific user.
+	 * 
+	 * @param toUser The user to recieve the message.
+	 * @param userMessage The message sent.
+	 * @throws IOException
+	 */
 	public void saveToMessage(String toUser, UserMessage userMessage) throws IOException {
 		File yourFile = new File("Messages/"+toUser+".txt");
 		if(!yourFile.exists()) {
 		    yourFile.createNewFile();
 		}
 
-		saveToUser(userMessage, yourFile);
+		saveToUserMessage(userMessage, yourFile);
 	}
-
+	/**
+	 * Gets five previous messages sent to the user.
+	 * 
+	 * @param username The username of the user.
+	 * @return LinkedList<String> with the messages.
+	 * @throws IOException
+	 */
 	public LinkedList<String> readMessages(String username) throws IOException
 	{
 		LinkedList<String> messages = new LinkedList<String>();
@@ -93,6 +138,11 @@ public class Logger{
 		return messages;
 	}
 
+	/**
+	 * Class used to update the user with previous messages.
+	 * 
+	 * @author Hiplobbe
+	 */
 	public class MessageUpdater extends Thread
 	{
 		private User user;
