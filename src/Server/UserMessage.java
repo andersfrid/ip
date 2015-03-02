50902d26d2aa905a66668934d8865b9eb644f5a4
hiplobbe <hiplobbe2@hotmail.com>
2015-03-02 11:42:38 +0100

    Added basic usermessage

---
+++ src/Server/UserMessage.java
@@ -0,0 +1,40 @@
+package Server;
+
+import javax.swing.Icon;
+
+import Interfaces.iMessage;
+
+public class UserMessage implements iMessage {
+	private String TextMessage;
+	private Icon Image;
+	private boolean IsPublic;
+	
+	@Override
+	public String getMessage() {
+		return TextMessage;
+	}
+	@Override
+	public void setMessage() {
+		
+	}
+	@Override
+	public Icon getImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public void setImage() {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public boolean IsPublic() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public String ToUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
