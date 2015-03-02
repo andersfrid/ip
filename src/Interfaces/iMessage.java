d67c666f9c10bfc34e0143521a7de68984f8de77
hiplobbe <hiplobbe2@hotmail.com>
2015-03-02 11:44:09 +0100

    Changes to iMessage

--- src/Interfaces/iMessage.java
+++ src/Interfaces/iMessage.java
@@ -4,11 +4,13 @@ import javax.swing.Icon;
 
 public interface iMessage {
 	public String getMessage();
-	public void setMessage();
+	public void setMessage(String message);
 	
 	public Icon getImage();
-	public void setImage();
+	public void setImage(Icon image);
 	
 	public boolean IsPublic();
+	
 	public String ToUser();
+	public void setTo(String username);
 }
