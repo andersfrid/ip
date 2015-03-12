5535b037698fc0cbce42f6960000c17efbfee4ec
Hiplobbe <hiplobbe2@hotmail.com>
2015-03-12 17:04:17 +0100

    no message

--- src/server/MainServer.java
+++ src/server/MainServer.java
@@ -2,6 +2,7 @@ package server;
 
 import interfaces.iMessage;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
@@ -15,166 +16,164 @@ public class MainServer {
 	private ServerSocket connSocket;
 	private int outPort = 3520;
 	private ClientListener listener = new ClientListener();
-	private LinkedList<User> userList = new LinkedList<User>();
-	
-	public MainServer() throws IOException
-	{
+	private LinkedList<User> userList;
+
+	public MainServer() throws IOException {
 		connSocket = new ServerSocket(3520, 0, InetAddress.getByName(null));
+
+		userList = fillUserList();
+
 		listener.start();
 	}
-	
+
+	private LinkedList<User> fillUserList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
 	public static void main(String[] args) throws IOException {
 		new MainServer();
-		new ClientGUI(new ClientController("127.0.0.1",3520));
+		new ClientGUI(new ClientController("127.0.0.1", 3520));
 	}
-	
-	private class MessageHandler extends Thread
-	{
+
+	private class MessageHandler extends Thread {
 		private UserMessage userMessage;
 		private Logger logger = new Logger();
 
 		public MessageHandler(iMessage message) {
 			userMessage = (UserMessage) message;
 		}
-		
+
 		@Override
 		public void run() {
-			while(!isInterrupted())
-			{
-				if(userMessage.ToUser().isEmpty())
-				{
+			while (!isInterrupted()) {
+				if (userMessage.ToUser().isEmpty()) {
 					try {
-						sendToAll();	
+						sendToAll();
 						logger.saveAllMessage(userMessage);
-					} 
-					catch (IOException e) {
+					} catch (IOException e) {
 						e.printStackTrace();
-					}		
-				}
-				else if(!userMessage.ToUser().isEmpty())
-				{
+					}
+				} else if (!userMessage.ToUser().isEmpty()) {
 					LinkedList<String> listOfUsers = userMessage.ToUser();
-					
-					for(String user : listOfUsers)
-					{
-						User u = findUser(user);
-						
-						if(u != null)
-						{
-							try {
-								sendToSpecific(u,userMessage);
-								
-								logger.saveToMessage(u.Username,userMessage);
-							} 
-							catch (IOException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
+
+					for (String user : listOfUsers) {
+						try {
+							User u = findUser(user);
+
+							if (u != null && u.OutStream != null) {
+
+								sendToSpecific(u, userMessage);
+
+								logger.saveToMessage(u.Username, userMessage);
 							}
+							else {
+								logger.saveToMessage(u.Username,userMessage);
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
 					}
 				}
-				
+
 				interrupt();
 			}
 		}
 
-		private void sendToSpecific(User u, UserMessage message) throws IOException {
-			synchronized (u.OutStream) {
-				u.OutStream.writeObject(message);
-				u.OutStream.flush();
+		private void sendToSpecific(User to, UserMessage message)
+				throws IOException {
+			synchronized (to.OutStream) {
+				to.OutStream.writeObject(message);
+				to.OutStream.flush();
 			}
-					
-			logger.logMessage(userMessage.getUsername(),u.Username,userMessage.getMessage());
+
+			logger.logMessage(userMessage.getUsername(), to.Username,
+					userMessage.getMessage());
 		}
 
-		private User findUser(String username) {
-			for(User u : userList)
-			{
-				if(u.Username == username)
-				{
+		private User findUser(String username) throws IOException {
+			for (User u : userList) {
+				if (u.Username == username) {
 					return u;
 				}
 			}
 
-			Logger logger = new Logger();
-			
-			try 
-			{
-				logger.logError(username);
-			} 
-			catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+			File yourFile = new File("Messages/" + username + ".txt");
+			if (!yourFile.exists()) {
+				try {
+					logger.logError(username);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				return null;
 			}
-			
-			return null;
+			else
+			{
+				return new User(username,null,null);
+			}	
 		}
 
 		private void sendToAll() throws IOException {
-			for(User u : userList)
-			{
+			for (User u : userList) {
 				synchronized (u.OutStream) {
 					u.OutStream.writeObject(userMessage);
 					u.OutStream.flush();
 				}
-				
-				logger.logMessage(userMessage.getUsername(),null,userMessage.getMessage());
+
+				logger.logMessage(userMessage.getUsername(), null,
+						userMessage.getMessage());
 			}
 		}
 	}
-	
-	private class ClientListener extends Thread
-	{
+
+	private class ClientListener extends Thread {
 		private Socket socket;
-		
+
 		@Override
 		public void run() {
-			while(!isInterrupted())
-			{
-				try 
-				{
+			while (!isInterrupted()) {
+				try {
 					System.out.println("startar asdadasdasd");
 					socket = connSocket.accept();
-					
-					ObjectOutputStream outStream = new ObjectOutputStream(socket.getOutputStream());
-					ObjectInputStream inStream = new ObjectInputStream(socket.getInputStream());
 
-					Object obj =  inStream.readUTF();
+					// ObjectOutputStream outStream = new
+					// ObjectOutputStream(socket.getOutputStream());
+
+					ObjectInputStream inStream = new ObjectInputStream(
+							socket.getInputStream());
+
+					Object obj = inStream.readUTF();
 					System.out.println(obj);
-					
-					if(obj instanceof String)
-					{
-						//TODO Kolla om användare redan finns!
-						//TODO Bry dig inte om dom är online eller inte.
-						User newUser = new User((String)obj,socket.getInetAddress(), outStream);
-										
+
+					if (obj instanceof String) {
+						// TODO Kolla senaste meddelande.
+						User newUser = new User((String) obj,socket.getInetAddress(),socket.getOutputStream());
+
 						userList.add(newUser);
-						updateClientLists();	
-					}
-					else if(obj instanceof iMessage)
-					{
-						MessageHandler handler = new MessageHandler((iMessage)obj);
+						// TODO Ta bort updateClientLists();?
+					} else if (obj instanceof iMessage) {
+						MessageHandler handler = new MessageHandler(
+								(iMessage) obj);
 						handler.start();
 					}
-				} 
-				catch (IOException e) {
-					// TODO Auto-generated catch block
+				} catch (IOException e) {
 					e.printStackTrace();
 				} catch (Exception e) {
-					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
 			}
 		}
 
-		private void updateClientLists() throws IOException {			
-			for(User u : userList)
-			{
-				synchronized (u.OutStream) {
-					u.OutStream.writeObject(userList);
-					u.OutStream.flush();
-				}
-			}
-		}
+//		private void updateClientLists() throws IOException {
+//			for (User u : userList) {
+//				synchronized (u.OutStream) {
+//					u.OutStream.writeObject(userList);
+//					u.OutStream.flush();
+//				}
+//			}
+//		}
 	}
 }
--- src/server/User.java
+++ src/server/User.java
@@ -1,17 +1,78 @@
 package server;
 
+import java.io.File;
+import java.io.IOException;
 import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.net.InetAddress;
+import java.util.ArrayList;
 
 public class User {
 	public String Username;
 	public InetAddress Address;
 	public ObjectOutputStream OutStream;
 	
-	public User(String name,InetAddress address, ObjectOutputStream stream)
+	public User(String name,InetAddress address, OutputStream stream) throws IOException
 	{
 		Username = name;
 		Address = address;
-		stream = OutStream;
+		OutStream = new ObjectOutputStream(stream);
+		
+		CheckMessageFile();
+	}
+
+	private void CheckMessageFile() {
+		File dir = new File("Messages");
+
+		if (!dir.exists()) {
+			dir.mkdir();
+		}
+		
+		File file = new File("Messages/" + Username + ".txt");
+		
+		if (!file.exists()) {
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		else if(OutStream != null)
+		{
+			try {
+				MessageUpdater update = new MessageUpdater();
+				update.start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	/**
+	 * Class used to update the user with previous messages.
+	 * 
+	 * @author Hiplobbe
+	 */
+	public class MessageUpdater extends Thread {
+		@Override
+		public void run() {
+			try {		
+				Logger log = new Logger();
+				ArrayList<String> list = log.checkMessages(Username);
+				
+				if (list.size() > 0) {
+					synchronized (OutStream) {
+						OutStream.writeObject(list);
+						OutStream.flush();
+					}
+				}
+
+				interrupt();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
